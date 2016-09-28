32ac92b5f6bc17dc1d9b81c03f0f55130a9c9ad1
tsatsa <tsatsa@57e885da-5824-4e83-b6ed-0c75d762b236>
2016-09-28 00:01:28 +0000

    git-svn-id: https://svn.riouxsvn.com/world-quests@535 57e885da-5824-4e83-b6ed-0c75d762b236

--- updater.bat
+++ updater.bat
@@ -1,4 +1,4 @@
 @echo off
 echo. Updating from SVN...
-"TortoiseProc.exe" /command:update /path:"./world-quests/" /closeonend:1
+"TortoiseProc.exe" /command:update /path:"./Default Profiles/world-quests/" /closeonend:1
 echo. done.
